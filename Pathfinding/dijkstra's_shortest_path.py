705276dedcb91e24b2825d0516a7e747975922ae
Crypticsy <animesh19basnet@gmail.com>
2020-08-11 17:00:03 +0545

    Codegolf

--- Pathfinding/dijkstra's_shortest_path.py
+++ Pathfinding/dijkstra's_shortest_path.py
@@ -1,31 +1,20 @@
 class dijkstra_shortest_path():
-
-    #attributes
-    graph, distance, final_dist = [{} for x in range(3)]
-
+    graph, distance, final_dist = [{} for x in range(3)]     # attributes
     
-    def findmin(this,dict):
-        minkey,minval = -1,float('inf')
-        for i in list(dict):
-            if this.final_dist[i] < minval:
-                minkey, minval = i, this.final_dist[i]
-        return minkey
+    def findmin(self,dict):
+        return min(list(dict), key = lambda x:this.final_dist[x])
 
-
-    
-    # Graph search
-    def __init__(this, graph, distance):
-        this.graph = graph
-        this.distance = distance
+    def __init__(self, graph, distance):        # initialization
+        this.graph, this.distance = graph, distance
         
-
-    def find_path(this, start, end):
+    def find_path(self, start, end):
         this.final_dist = {x:float('inf') for x in this.graph.keys()}
-        visited,inlist = set(),set()
+        visited, inlist = set(), set()
+
         inlist.add(start)
         this.final_dist[start] = 0
 
-        while inlist!=set():
+        while inlist != set():
             currkey = this.findmin(inlist)
             visited.add(currkey)
             inlist.discard(currkey)
@@ -33,34 +22,7 @@ class dijkstra_shortest_path():
             for i in this.graph[currkey]:
                 if i not in visited:
                     currval = this.final_dist[currkey] + this.distance[(currkey,i)]
-                    this.final_dist[i] = min(this.final_dist[i],currval)
+                    this.final_dist[i] = min(this.final_dist[i], currval)
                     inlist.add(i)
         
         return this.final_dist[end]
-            
-                
-
-
-
- #Testing
-
-values = open('d:/Learning/Stanford Algo Class/2. Graph Search, Shortest Paths, and Data Structures/dijkstraData.txt','r')
-
-tempgraph, tempdist = {},{}
-for i in values:
-    val = i.split()
-    
-    node1 = val.pop(0) 
-    tempgraph.setdefault(node1,[])
-    for a in val:
-        node2,dist = a.split(',')
-        tempgraph[node1].append(node2)
-        tempdist[(node1,node2)] = int(dist)
-    
-
-test1 = dijkstra_shortest_path(tempgraph,tempdist)
-start = '1'
-
-
-for i in [7,37,59,82,99,115,133,165,188,197]:
-    print("Shortest distance from",start,"to",i,"is  : ",test1.find_path(start,str(i)))
